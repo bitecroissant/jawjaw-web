5a2daafcf7fa41779eb6f776a0a392ce52a83287
bitecroissant <bitecroissant@hotmail.com>
2024-12-13 21:49:17 +0800

    部署单独看一下html loading效果

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,14 +1,27 @@
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, reactive } from 'vue';
 import { useRouter } from 'vue-router';
+import s from "./Home.module.scss";
 export const Home = defineComponent({
   setup: () => {
+    const pageData = reactive({ text: '🧨 昭昭如愿，岁岁安澜' })
     const router = useRouter()
     const logout = () => {
       router.replace('/sign_in')
     }
+    onMounted(() => {
+      // setTimeout(() => {
+      //   pageData.text = '人生海海，山山而川，不过尔尔'
+      // }, 2300)
+    });
     return () => (
-      <div h-vhcheck flex justify-center items-center>
-        <button p-24px onClick={logout}>退出登录</button>
+      <div h-vhcheck flex justify-center items-center flex-col>
+        <div></div>
+        <button p-24px mb-48px onClick={logout}>退出登录</button>
+        <div class={s.greetingTextWrapper}>
+          <h3 data-glitch={pageData.text} class={s.greetingText} >
+            {pageData.text}
+          </h3>
+        </div>
       </div>
     )
   }
