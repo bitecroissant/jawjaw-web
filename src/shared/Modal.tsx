2439986a8bb8c8de8c748edd17f72ef18736e90b
bitecroissant <bitecroissant@hotmail.com>
2024-12-15 00:37:28 +0800

    完成modal

---
+++ src/shared/Modal.tsx
@@ -0,0 +1,31 @@
+import { defineComponent, PropType, ref, Teleport, Transition } from 'vue';
+import s from './Modal.module.scss';
+import { onClickOutside } from '@vueuse/core';
+export const Modal = defineComponent({
+  props: {
+    modalVsible: Boolean,
+    close: Function
+  },
+  setup: (props, context) => {
+    const {slots} = context
+    const refModal = ref(null)
+    onClickOutside(refModal, () => {
+      props.close && props.close()
+    })
+    return () => (
+      <Teleport to="#modal">
+        <Transition enter-from-class={s.modalEnterFrom}
+          enter-active-class={s.modalEnterActive}
+          leave-to-class={s.modalLeaveTo}
+          leave-active-class={s.modalLeaveActive}
+        >
+          <div v-show={props.modalVsible} class={s.modalBg} h-vhcheck>
+            <div ref={refModal} class={s.modal}>
+              {slots.default?.()}
+            </div>
+          </div>
+        </Transition>
+      </Teleport>
+    )
+  }
+})
