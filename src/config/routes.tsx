c96e351458b521f1a473cbac908659703a90655a
bitecroissant <bitecroissant@hotmail.com>
2024-12-14 19:06:15 +0800

    自动路由home

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -2,6 +2,7 @@ import { Home } from '../views/Home'
 import { SignInPage } from '../views/SginInPage'
 
 export const routes = [
+    { path: '/', redirect: '/home' },
     { path: '/sign_in', component: SignInPage },
     { path: '/home', component: Home },
 ]
